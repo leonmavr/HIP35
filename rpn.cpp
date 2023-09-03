7ca58a2f83523bb646b67be1c7092bb1124787df
leonmavr <0xleo.git@gmail.com>
2023-09-03 22:07:22 +0200

    Design object relationships and RPN calculator's interface

--- rpn.cpp
+++ rpn.cpp
@@ -1,12 +1,5 @@
 #include "rpn.hpp"
 
-void RpnStack::rdn() {
-    auto oldFirst = stack_[0];
-    for (int i = 0; i < stack_.size()-1; ++i)
-        stack_[i] = stack_[i+1];
-    stack_[stack_.size()] = oldFirst;
-}
-
 
 void RpnStack::shiftUp() {
     for (int i = stack_.size()-1; i > 0; --i)
@@ -18,4 +11,27 @@ void RpnStack::shiftDown() {
     for (int i = 0; i < stack_.size()-1; ++i)
         stack_[i] = stack_[i+1];
     stack_[stack_.size()-1] = 0.0;
+}
+
+void RpnBackend::rdn() {
+    auto oldFirst = stack_[0];
+    for (int i = 0; i < stack_.size()-1; ++i)
+        stack_[i] = stack_[i+1];
+    stack_[stack_.size()] = oldFirst;
+}
+
+
+void RpnBackend::insert(double num) {
+    if (!is_init_) {
+        is_init_ = true;
+    } else {
+        stack_.shiftUp();
+    }
+    stack_.writeX(num);
+}
+
+
+void RpnBackend::enter() {
+    stack_.shiftUp();
+    stack_[IDX_REG_X] = stack_[IDX_REG_Y];
 }
--- rpn.hpp
+++ rpn.hpp
@@ -1,6 +1,5 @@
 #ifndef RPN_HPP
-#define RPN_HPP 
-
+#define RPN_HPP
 #include <array>
 
 enum {
@@ -10,26 +9,57 @@ enum {
     IDX_REG_T,
 };
 
+class RpnBase {
+public:
+    virtual void swapXY() = 0;
+    virtual double peek() const = 0;
+    virtual void insert(double num) = 0;
+    virtual void rdn() = 0;
+    virtual void enter() = 0;
+};
+
+class RpnBackend;
+
 class RpnStack {
-    public:
-        RpnStack() { stack_ = {0., 0., 0., 0.}; }; 
-        ~RpnStack() { }; 
-        double peek() const { return stack_[IDX_REG_X]; };
-        void swapXY() { std::swap(stack_[IDX_REG_X], stack_[IDX_REG_Y]); };
-        void rdn();
-        void writeX(double x) { stack_[IDX_REG_X] = x; };
-        void shiftUp();
-        void shiftDown();
-        void clear() { stack_ = {0., 0., 0., 0.}; }
-        std::array<double, 4> stack_;
-    private:
+public:
+    RpnStack() { clear(); }
+    ~RpnStack() {}
+
+    void shiftUp();
+    void shiftDown();
+    void clear() { stack_ = { 0., 0., 0., 0. }; }
+    void writeX(double x) { stack_[IDX_REG_X] = x; }
+    // index getter operator
+    double operator[] (double i) const { return stack_[i]; }
+    // index setter operator
+    double& operator[] (double i) { return stack_[i]; }
+    unsigned size() const { return stack_.size(); }
+
+protected:
+    std::array<double, 4> stack_;
+private:
+    friend class RpnBackend;
 };
 
-class RpnBackend {
-    public:
-    private:
+class RpnBackend: RpnBase {
+public:
+    RpnBackend():
+        is_init_(false)
+    {}
+    ~RpnBackend() {}
+    // Make RpnStack a friend class
+    //friend class RpnStack;
+    virtual void swapXY() { std::swap(stack_[IDX_REG_X], stack_[IDX_REG_Y]); }
+    virtual double peek() const { return stack_[IDX_REG_X]; }
+    virtual void insert(double num);
+    virtual void rdn();
+    virtual void enter();
 
+private:
+    // Move any private members you want to be accessible to RpnStack to here
+    RpnStack stack_;
+    bool is_init_;
 };
 
+#endif
 
-#endif /* RPN_HPP */
