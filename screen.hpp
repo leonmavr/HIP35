0dfe124e8eb1fbb503ab0f695490a74144f2a32f
0xleo <0xleo.git@gmail.com>
2023-09-10 11:05:52 +0300

    WIP for another computer

---
+++ screen.hpp
@@ -0,0 +1,17 @@
+#ifndef SCREEN_HPP
+#define SCREEN_HPP 
+
+namespace Gui {
+class Screen
+{
+    public:
+        Screen () {};
+        ~Screen () {};
+
+    private:
+        /* data */
+};
+
+}
+
+#endif /* SCREEN_HPP */
