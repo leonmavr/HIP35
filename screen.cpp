8a1e9be452088d41521bda877982c4023cc646a3
leonmavr <0xleo.git@gmail.com>
2023-09-12 10:55:08 +0200

    Store keypad grid in a better structure

--- screen.cpp
+++ screen.cpp
@@ -1,27 +1,57 @@
 #include "screen.hpp"
+#include <utility> // make_pair
 
 Gui::Screen::Screen(std::vector<std::string> keys):
-	active_key_("") {
-	MapTextToKey();
+	active_key_(""),
+    key_width(8),
+    key_height(1)
+{
+	InitKeypadGrid();
 }
 
-void Gui::Screen::MapTextToKey() {
+/**
+ * @brief Arranges the keypad given the key mappings (`key_mappings_`).
+ *        In the end, we want to arrange the keypad as a 2D grid that
+ *        looks as follows:
+ *
+ *        The screen requires 2 rows as we want to draw a RPN calculator.
+ *        The screen displays:
+ *        register Y
+ *        register X
+ *        
+ *        Screen
+ *        |
+ *        | 
+ *        v
+ *        +----------------------------------- 
+ *        | 4.00            
+ *        | 20.00
+ *        +----------------------------------- <- Keypad
+ *        | +       | -       | *        | ...
+ *        +----------------------------------- 
+ *        | sin (s) | cos (c) | tan (t)  | ...
+ *        +----------------------------------- 
+ *        ...
+ */
+void Gui::Screen::InitKeypadGrid() {
 	// arithmetic operations
-	key_mappings_["log10"] = "l";
-	key_mappings_["log"] = "l";
-	key_mappings_["sin"] = "s";
-	key_mappings_["cos"] = "c";
-	key_mappings_["tan"] = "t";
-	key_mappings_["sqrt"] = "S";
-	key_mappings_["chs"] = "C";
-	key_mappings_["inv"] = "i";
-	key_mappings_["+"] = "+";
-	key_mappings_["-"] = "-";
-	key_mappings_["*"] = "*";
-	key_mappings_["/"] = "/";
-	key_mappings_["^"] = "^";
+	key_mappings_["+"] =     std::make_pair("+", Gui::Point{0, 2});
+	key_mappings_["-"] =     std::make_pair("-", Gui::Point{1, 2});
+	key_mappings_["*"] =     std::make_pair("*", Gui::Point{2, 2});
+	key_mappings_["/"] =     std::make_pair("/", Gui::Point{3, 2});
+	key_mappings_["^"] =     std::make_pair("^", Gui::Point{4, 2});
+
+	key_mappings_["sin"] =   std::make_pair("s", Gui::Point{0, 2});
+	key_mappings_["cos"] =   std::make_pair("c", Gui::Point{1, 2});
+	key_mappings_["tan"] =   std::make_pair("t", Gui::Point{2, 2});
+	key_mappings_["sqrt"] =  std::make_pair("S", Gui::Point{3, 2});
+	key_mappings_["log"] =   std::make_pair("l", Gui::Point{4, 2});
+
+	key_mappings_["chs"] =   std::make_pair("C", Gui::Point{0, 2});
+	key_mappings_["inv"] =   std::make_pair("i", Gui::Point{1, 2});
+    
 	// HP35 stack operations
-	key_mappings_["RND"] = "v";
-	key_mappings_["SWAP"] = "<";
-	key_mappings_["LASTX"] = "x";
+	key_mappings_["RND"] =   std::make_pair("v", Gui::Point{0, 2});
+	key_mappings_["SWAP"] =  std::make_pair("(", Gui::Point{1, 2});
+	key_mappings_["LASTX"] = std::make_pair("x", Gui::Point{2, 2});
 }
--- screen.hpp
+++ screen.hpp
@@ -1,26 +1,36 @@
 #ifndef SCREEN_HPP
 #define SCREEN_HPP 
 
-#include <unordered_map>
-#include <string>
-#include <vector>
+#include <unordered_map> // unordered_map
+#include <string> // string
+#include <vector> // vector
+#include <utility> // pair
 
 namespace Gui {
+
+/**
+* @brief Point in the keypad grid with top left as origin (0, 0) 
+*/
+typedef struct {
+    unsigned x, y;
+} Point;
+
 class Screen
 {
-    public:
-        Screen () = delete;
+public:
+    Screen () = delete;
 	// TODO
-        Screen (std::vector<std::string> keys);
-        ~Screen () {};
+    Screen (std::vector<std::string> keys);
+    ~Screen () {};
 	// TODO
 	void Draw();
 
-    private:
-	std::unordered_map<std::string, std::string> key_text_;
-	std::unordered_map<std::string, std::string> key_mappings_;
+private:
+	std::unordered_map<std::string, std::pair<std::string, Point>> key_mappings_;
 	std::string active_key_;
-	void MapTextToKey();
+	void InitKeypadGrid();
+    unsigned key_width;
+    unsigned key_height;
 };
 
 }
