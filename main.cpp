e3836e25d4472ca16c3f2a31114e3403a05c306c
leonmavr <0xleo.git@gmail.com>
2023-09-20 14:13:18 +0200

    WIP: come up with a way to chain the frontend and backend

--- main.cpp
+++ main.cpp
@@ -3,11 +3,35 @@
 #include "observer.hpp"
 #include "screen.hpp"
 #include <iostream>
+#include <sstream>
 #include <string>
+
+static std::vector<std::string> SplitStringBySpace(const std::string& string) {
+    // space-delimited tokens to return
+    std::vector<std::string> tokens;
+    // space-delimited stream
+    std::istringstream iss(string);
+    while (iss) {
+        std::string token;
+        iss >> token;
+        if (!token.empty())
+            tokens.push_back(token);
+    }
+    return tokens;
+}
 
 int main() {
     auto obs1 = Observer();
     auto rpn = std::make_unique<Rpn::Backend>();
+    rpn->Attach(&obs1);
+    std::string rpn_string = "2 5 * 4 + 3 2 * 1 + /";
+    auto tokens = SplitStringBySpace(rpn_string);
+    // calculator needs enter to be pressed to separate two successive numbers
+    bool press_enter = false;
+    for (const auto& token: tokens) {
+        
+    }
+
 #if 0
     // 2 5 * 4 + 3 2 * 1 + /
     rpn->Attach(&obs1);
@@ -30,11 +54,11 @@ int main() {
     //std::cout << "---" << rpn.calculateFromString("2 1.4 /") << std::endl;
     std::cout << *rpn << std::endl;
     std::cout << rpn->GetFunctions()[0] << std::endl;
-#endif
     auto scr = Gui::Frontend();
     scr.DrawKey("+", true);
     scr.DrawKey("t", true);
     scr.DrawKey("t");
     scr.PrintRegisters(4.01, 20.0001);
     getchar();
+#endif
 }
