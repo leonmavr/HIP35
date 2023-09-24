abc937d9b5f73b7aec860ed35b92527e886da999
leonmavr <0xleo.git@gmail.com>
2023-09-25 01:40:40 +0200

    Add a sketch of the loop

--- hip35.cpp
+++ hip35.cpp
@@ -11,12 +11,35 @@ Hip35::Hip35(std::unique_ptr<Gui::Frontend> frontend,
     backend->Attach(observer.get());
 }
 
+static bool IsDecimal(const std::string& str) {
+    try {
+        size_t pos;
+        std::stod(str, &pos);
+        // Make sure the entire string was used for conversion
+        return pos == str.length();
+    } catch (const std::invalid_argument&) {
+        return false;
+    } catch (const std::out_of_range&) {
+        return false;
+    }
+}
+
 void Hip35::RunUI() {
     char input_char;
-    std::string token;
+    std::string token = "";
     std::vector<std::string> functions = backend_->GetFunctions();
     bool previous_token_is_digit = false;
     while (1) {
         std::cin >> input_char;
+        // check if it is space
+        if (input_char == ' ') {
+            // look at the token and decide if it's a number or function
+            if (backend_->IsInFunctions(token)) {
+                // process calculation stuff
+            } else if (IsDecimal(token)) {
+                // process insertion stuff
+            }
+            token = "";
+        }
     }
 }
