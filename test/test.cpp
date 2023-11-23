cbbff4af7296fdf275282d902177a5b50f2c7ef5
leonmavr <mvrleo@gmail.com>
2023-11-23 23:04:19 +0100

    make sure to use cpp17

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,21 @@
+#include "hip35.hpp"
 #include <iostream>
+#include <cassert>
 
+#define ASSERT_FLOAT_CLOSE(actual, expected, epsilon) \
+    do { \
+        float actualValue = (actual); \
+        float expectedValue = (expected); \
+        float epsilonValue = (epsilon); \
+        if (!(std::fabs(actualValue - expectedValue) < epsilonValue)) { \
+            std::cerr << "Assertion failed: " #actual " and " #expected " not close at line " << __LINE__ << std::endl; \
+        } \
+    } while(0)
 
 int main() {
+	ASSERT_FLOAT_CLOSE(1, 1, 0.01);
+	ASSERT_FLOAT_CLOSE(1, 2, 0.01);
+	ASSERT_FLOAT_CLOSE(1, 1, 0.01);
 	std::cout << "running tests\n";
 
 }
