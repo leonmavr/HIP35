c6e82a2bdfb54f3adf0ca402198f3380b28a3482
leonmavr <0xleo.git@gmail.com>
2023-12-02 19:49:57 +0100

    Better error reporting in nanotest

--- test/nanotest.h
+++ test/nanotest.h
@@ -1,11 +1,25 @@
+/*
+ * This is a tiny header-only unit testing framework for
+ * C and C++. It is compatible with GNU-based compilers
+ * and has not been tested in anything else.
+ * Author: L Mavropalias 2023
+ */
 extern "C" {
 
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
 
 #define NTEST_PASS 0
 #define NTEST_FAIL 1
 int ntest_result = NTEST_PASS;
+
+// credits @red1ynx (https://stackoverflow.com/a/8488201)
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+// stringification macros
+#define STR_VAL(s) STR_NAME(s)
+#define STR_NAME(s) #s
 
 #define GET_FIRST(args) GET_FIRST_(args)
 #define GET_FIRST_(N, ...) N 
@@ -22,34 +36,36 @@ int ntest_result = NTEST_PASS;
 #define CHECK_EMPTY(default_val, ...) (default_val CHECK_EMPTY_(__VA_ARGS__))
 #define CHECK_EMPTY_(...) ,##__VA_ARGS__
 
+// unit test macros
 #define NTEST_ASSERT_FLOAT_CLOSE(...) NTEST_ASSERT_FLOAT_CLOSE_(__VA_ARGS__)
-#define NTEST_ASSERT_FLOAT_CLOSE_(actual, expected, ...) \
-    do { \
-        const float actual_value = (actual); \
-        const float expected_value = (expected); \
-        float epsilon = 0.0001; \
-        if (COUNT_ARGS(__VA_ARGS__) == 1) \
-           epsilon = CHECK_EMPTY(epsilon, GET_FIRST(__VA_ARGS__)); \
-        if (fabs(actual_value - expected_value) < epsilon) \
-            printf("[v] Assertion at line %d passed.\n", __LINE__); \
-        else { \
-            fprintf(stderr, "[x] Assertion at line %d of file %s failed:\n" \
-                            "    Values of %s and %s are not close\n", \
-                    __LINE__, __FILE__, #actual, #expected); \
-            ntest_result = NTEST_FAIL; \
-        } \
+#define NTEST_ASSERT_FLOAT_CLOSE_(actual, expected, ...)                     \
+    do {                                                                     \
+        const float actual_value = (actual);                                 \
+        const float expected_value = (expected);                             \
+        float epsilon = 0.0001;                                              \
+        if (COUNT_ARGS(__VA_ARGS__) == 1)                                    \
+           epsilon = CHECK_EMPTY(epsilon, GET_FIRST(__VA_ARGS__));           \
+        if (fabs(actual_value - expected_value) < epsilon)                   \
+            printf("[v] Assertion at line %d passed.\n", __LINE__);          \
+        else {                                                               \
+            fprintf(stderr, "[x] Assertion at line %d of file %s failed:\n"  \
+                            "    Values of %s=%s and %s=%s are not close\n", \
+                    __LINE__, __FILENAME__, #actual, STR_VAL(actual),        \
+                    #expected, STR_VAL(expected));                           \
+            ntest_result = NTEST_FAIL;                                       \
+        }                                                                    \
     } while(0)
 
 
-#define NTEST_ASSERT(condition) \
-    do { \
-        if (condition) \
-            printf("[v] Assertion at line %d passed.\n", __LINE__); \
-        else { \
-            fprintf(stderr, "[x] Assertion at line %d of file %s failed" \
-                            ":\n    %s", __LINE__, #condition); \
-            ntest_result = NTEST_FAIL; \
-        } \
+#define NTEST_ASSERT(condition)                                               \
+    do {                                                                      \
+        if (condition)                                                        \
+            printf("[v] Assertion at line %d passed.\n", __LINE__);           \
+        else {                                                                \
+            fprintf(stderr, "[x] Assertion at line %d of file %s failed:\n"   \
+                            "    %s\n", __LINE__, __FILENAME__, #condition);  \
+            ntest_result = NTEST_FAIL;                                        \
+        }                                                                     \
     } while(0)
 
 } // extern "C"
