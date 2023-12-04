b2b1e932b537c0f2d89d52a532a1dc3001a3a104
leonmavr <0xleo.git@gmail.com>
2023-12-04 01:58:20 +0100

    Remove unnecessary includes from main

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,9 +1,5 @@
-#include "stack.hpp"
-#include "backend.hpp"
-#include "observer.hpp"
-#include "frontend.hpp"
 #include "hip35.hpp"
-#include "keypad.hpp"
+#include "keypad.hpp" // Key::keypad
 
 int main() {
     auto hp = std::make_unique<Ui::Hip35>(Key::keypad);
